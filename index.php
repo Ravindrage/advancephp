819ec099c19ac9ffad75a2844a18b2ea14cdcaf4
Ravindra <abtechreair1@gmail.com>
2024-06-03 15:05:18 +0530

    Today work

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+$name_9 = "Hariom" ; 
+$number = 10 ; 
+$percent = 10.15 ;
+
+
+echo  $name_9;  echo '</br>';
+echo $number; echo '</br>';
+echo $percent;
+
+//$$hindi = $number ; 
+
+//echo $$hindi ; 
+//////////////-Index Array ////////
+/* -------------  Array Index array Example   -----------*/
+
+$index_marks = array(80,90,99,71,80) ; 
+
+/* echo '<pre>' ; 
+print_r($index_marks); 
+echo '</pre>' ; 
+
+echo $index_marks[4];
+
+/* -------------Associative array ------------------*/
+
+/*$Associative_marks = array("Ram"=>"90","Harish"=>"99");
+
+echo '<pre>' ; 
+print_r($Associative_marks); 
+echo '</pre>' ; 
+
+echo $Associative_marks["Ram"];
+
+/////*--------- Multidimensional Array --------------
+//		1 2 3
+//		4 5 6
+//		7 8 9
+//*/
+
+
+/*$multi_array  = array(
+						array("hc"=>"car","he"=>"bike","h"=>"cycle"),
+						array("halicopter","rocket","aeroplane"),
+						array("bajaj","Hero","yamaha"),
+						);
+						
+	
+echo '<pre>' ; 
+print_r($multi_array); 
+echo '</pre>' ; 
+
+echo $multi_array[0][2] ;	
+
+echo '</br>';
+echo $multi_array[1][1];
+echo '</br>';
+echo $multi_array[2]["lastname"];
+
+
+echo '</br>';
+
+echo $marks["Ram"];
+
+
+echo '</br>';
+
+$condition = Null ;
+
+
+//print
+//print_r
+
+
+
+*/
+
+
+
+
+class bike {
+	
+    $color ; 
+	$name	; 
+	
+	function setcolor($color2)
+	{
+		$this->color = $color2 ; 		
+	}
+	
+	function getname($name)
+	{
+		$this->name = $name
+	}
+		
+}
+
+
+
+
+
+
